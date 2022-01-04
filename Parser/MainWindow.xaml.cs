e7c3ffbcc48f92471db8c449c094c0a32eae0cc7
clackgot <clackgot@gmail.com>
2022-01-04 15:02:46 +0300

    Добавьте файлы проекта.

---
+++ Parser/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+//using System.Windows.Shapes;
+
+namespace Parser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Text Files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (sfd.ShowDialog() == true)
+            {
+                TextRange doc = new TextRange(docBox.Document.ContentStart, docBox.Document.ContentEnd);
+                using (FileStream fs = File.Create(sfd.FileName))
+                {
+                    if (Path.GetExtension(sfd.FileName).ToLower() == ".txt")
+                        doc.Save(fs, DataFormats.Text);
+                }
+            }
+        }
+
+        private void Load_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "RichText Files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == true)
+            {
+                TextRange doc = new TextRange(docBox.Document.ContentStart, docBox.Document.ContentEnd);
+                using (FileStream fs = new FileStream(ofd.FileName, FileMode.Open))
+                {
+                    if (Path.GetExtension(ofd.FileName).ToLower() == ".txt")
+                    {
+                        byte[] array = new byte[fs.Length];
+                        fs.Read(array, 0, array.Length);
+                        string textFromFile = Encoding.Default.GetString(array);
+
+                        Regex regex = new Regex(@"Password: .*?(\n|\z|\r)");
+                        var matches = regex.Matches(textFromFile);
+                        List<string> passwords = new List<string>();
+                        foreach (Match match in matches)
+                        {
+                            passwords.Add(match.Value.Replace("Password: ", ""));
+                        }
+                        if(passwords.Count == 0)
+                        {
+                            MessageBox.Show("Паролей не найдено");
+                            return;
+                        }
+
+                        int oldCount = passwords.Count;
+                        passwords = passwords.Distinct().ToList();
+                        int newCount = passwords.Count;
+                        MessageBox.Show($"Удалено {oldCount - newCount} дублей");
+                        string result = "";
+                        foreach (var password in passwords)
+                        {
+                            result += password;
+                        }
+                        doc.Text = result;
+
+                        //doc.Load(fs, DataFormats.Text);
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ PasswordsParser/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PasswordsParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex(@"Password: .*?(\n|\z|\r)");
+            string txt = File.ReadAllText("аа.txt");
+            var matches = regex.Matches(txt);
+            List<string> passwords = new List<string>();
+            foreach (Match match in matches)
+            {
+                passwords.Add(match.Value.Replace("Password: ", ""));
+            }
+            foreach (var password in passwords)
+            {
+                Console.WriteLine(password);
+            }
+        }
+    }
+}
